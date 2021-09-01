8f80e72f6ea6f7cf14a8d458277e4223585e055f
ShiftySlothe <forson.charlesj@gmail.com>
2021-09-01 17:25:12 +0100

    Added prod & dev keys for use with env variables

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
-module.exports = {
-  googleClientID:
-    "",
-  googleClientSecret: "",
-  mongoURI:
-    "",
-  cookieKey: "",
-};
+// Assigns creditentials based of prod/dev
+if (process.env.NODE_ENV === "production") {
+  // In prod
+  module.exports = require("./prod");
+} else {
+  // In dev
+  module.exports = require("./dev");
+}
